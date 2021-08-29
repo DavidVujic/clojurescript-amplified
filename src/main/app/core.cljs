4915fbee42029bde34a5b0316a955394892ada74
davidvujic <david@vujic.se>
2021-08-29 14:35:12 +0200

    Add AWS Amplify initialization

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,8 +1,11 @@
 (ns app.core
-  (:require [reagent.dom :as rdom]))
+  (:require ["/aws-exports" :default ^js aws-exports]
+            ["aws-amplify" :default Amplify]
+            [reagent.dom :as rdom]))
 
 (defn app []
   [:div "Hello World"])
 
 (defn ^:export main []
+  (-> Amplify (.configure aws-exports))
   (rdom/render [app] (-> js/document (.getElementById "root"))))
