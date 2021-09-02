3fa4d44bf62f11514ebb9ad7b73bc1509b14afd7
davidvujic <david@vujic.se>
2021-09-02 15:14:04 +0200

    refactor: no need to let the theme, used for one thing only in this example

--- src/main/app/components/song.cljs
+++ src/main/app/components/song.cljs
@@ -6,20 +6,18 @@
             [reagent-material-ui.styles :as styles]))
 
 (defn- fn-song-card []
-  (let [theme          (styles/use-theme)
-        right-to-left? (= (:direction theme) "rtl")]
+  (let [right-to-left? (-> styles/use-theme :direction (= "rtl"))]
     [card {:sx {:display "flex"}}
+
      [box {:sx {:display "flex" :flex-direction "column"}}
       [card-content {:sx {:flex "1 0 auto"}}
        [typography {:component "div" :variant "h5"} "Live from Space"]
        [typography {:component "div" :variant "subtitle1" :color "text.secondary"} "Mac Miller"]]
 
       [box {:sx {:display "flex" :align-items "center" :pl 1 :pb 1}}
-       [icon-button {:aria-label "previous"}
-        (if right-to-left? [skip-next] [skip-previous])]
+       [icon-button {:aria-label "previous"} (if right-to-left? [skip-next] [skip-previous])]
        [icon-button {:aria-label "play/pause"} [play-arrow]]
-       [icon-button {:aria-label "next"}
-        (if right-to-left? [skip-previous] [skip-next])]]]
+       [icon-button {:aria-label "next"} (if right-to-left? [skip-previous] [skip-next])]]]
 
      [card-media {:component "img"
                   :sx        {:width 151}
