e6a8a8043efbd7273232ecd04334cfb96e703143
davidvujic <david@vujic.se>
2021-09-02 14:32:43 +0200

    A song component using Material-UI + storybook stories

--- src/main/app/components/song.cljs
+++ src/main/app/components/song.cljs
@@ -1,27 +1,30 @@
 (ns app.components.song
-  (:require [reagent-material-ui.components :refer [box
-                                                    card
-                                                    card-content
-                                                    card-media
-                                                    icon-button
-                                                    typography]]
+  (:require [reagent-material-ui.components :refer [box card card-content card-media icon-button typography]]
             [reagent-material-ui.icons.play-arrow :refer [play-arrow]]
             [reagent-material-ui.icons.skip-next :refer [skip-next]]
-            [reagent-material-ui.icons.skip-previous :refer [skip-previous]]))
+            [reagent-material-ui.icons.skip-previous :refer [skip-previous]]
+            [reagent-material-ui.styles :as styles]))
+
+(defn- fn-song-card []
+  (let [theme          (styles/use-theme)
+        right-to-left? (= (:direction theme) "rtl")]
+    [card {:sx {:display "flex"}}
+     [box {:sx {:display "flex" :flex-direction "column"}}
+      [card-content {:sx {:flex "1 0 auto"}}
+       [typography {:component "div" :variant "h5"} "Live from Space"]
+       [typography {:component "div" :variant "subtitle1" :color "text.secondary"} "Mac Miller"]]
+
+      [box {:sx {:display "flex" :align-items "center" :pl 1 :pb 1}}
+       [icon-button {:aria-label "previous"}
+        (if right-to-left? [skip-next] [skip-previous])]
+       [icon-button {:aria-label "play/pause"} [play-arrow]]
+       [icon-button {:aria-label "next"}
+        (if right-to-left? [skip-previous] [skip-next])]]]
+
+     [card-media {:component "img"
+                  :sx        {:width 151}
+                  :image     "https://next.material-ui.com/static/images/cards/live-from-space.jpg"
+                  :alt       "Live from space album cover"}]]))
 
 (defn song-card []
-  [card {:sx {:display "flex"}}
-   [box {:sx {:display "flex" :flex-direction "column"}}
-    [card-content {:sx {:flex "1 0 auto"}}
-     [typography {:component "div" :variant "h5"} "Live from Space"]
-     [typography {:component "div" :variant "subtitle1" :color "text.secondary"} "Mac Miller"]]
-
-    [box {:sx {:display "flex" :align-items "center" :pl 1 :pb 1}}
-     [icon-button {:aria-label "previous"} [skip-previous]]
-     [icon-button {:aria-label "play/pause"} [play-arrow]]
-     [icon-button {:aria-label "next"} [skip-next]]]
-
-    [card-media {:component "img"
-                 :sx        {:width 151}
-                 :image     "https://next.material-ui.com/static/images/cards/live-from-space.jpg"
-                 :alt       "Live from space album cover"}]]])
+  [:f> fn-song-card])
--- src/stories/app/stories/song_stories.cljs
+++ src/stories/app/stories/song_stories.cljs
@@ -1,12 +1,11 @@
 (ns app.stories.song-stories
-  (:require [app.components.song :as song]
+  (:require [app.components.song :refer [song-card]]
             [app.stories.helper :as helper]
             [reagent.core :as reagent]))
 
 (def ^:export default
-  (helper/->default {:title     "A Material-UI Song Component"
-                     :component song/song-card}))
+  (helper/->default {:title     "Material-UI Song"
+                     :component song-card}))
 
 (defn ^:export song []
-  (reagent/as-element
-   [song/song-card]))
+  (reagent/as-element [song-card]))
