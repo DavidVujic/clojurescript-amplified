cf260de5162c7ea41e87be204ffd4212f34985fa
davidvujic <david@vujic.se>
2021-09-02 13:30:46 +0200

    WIP: a song component using Material-UI + storybook stories

--- src/main/app/components/song.cljs
+++ src/main/app/components/song.cljs
@@ -5,10 +5,23 @@
                                                     card-media
                                                     icon-button
                                                     typography]]
-            [reagent-material-ui.icons.skip-previous :refer [skip-previous]]
             [reagent-material-ui.icons.play-arrow :refer [play-arrow]]
             [reagent-material-ui.icons.skip-next :refer [skip-next]]
-            [reagent-material-ui.icons.info :refer [info]]))
+            [reagent-material-ui.icons.skip-previous :refer [skip-previous]]))
 
 (defn song-card []
-  [:div "hello world."])
+  [card {:sx {:display "flex"}}
+   [box {:sx {:display "flex" :flex-direction "column"}}
+    [card-content {:sx {:flex "1 0 auto"}}
+     [typography {:component "div" :variant "h5"} "Live from Space"]
+     [typography {:component "div" :variant "subtitle1" :color "text.secondary"} "Mac Miller"]]
+
+    [box {:sx {:display "flex" :align-items "center" :pl 1 :pb 1}}
+     [icon-button {:aria-label "previous"} [skip-previous]]
+     [icon-button {:aria-label "play/pause"} [play-arrow]]
+     [icon-button {:aria-label "next"} [skip-next]]]
+
+    [card-media {:component "img"
+                 :sx        {:width 151}
+                 :image     "https://next.material-ui.com/static/images/cards/live-from-space.jpg"
+                 :alt       "Live from space album cover"}]]])
--- src/stories/app/stories/song_stories.cljs
+++ src/stories/app/stories/song_stories.cljs
@@ -1,4 +1,12 @@
 (ns app.stories.song-stories
   (:require [app.components.song :as song]
+            [app.stories.helper :as helper]
             [reagent.core :as reagent]))
 
+(def ^:export default
+  (helper/->default {:title     "A Material-UI Song Component"
+                     :component song/song-card}))
+
+(defn ^:export song []
+  (reagent/as-element
+   [song/song-card]))
