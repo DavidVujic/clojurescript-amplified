8c1a6913beefb47cd95cec9c24ec2b8d63756da8
davidvujic <david@vujic.se>
2021-09-06 18:35:55 +0200

    Separate the AWS Amplify examples with an alias, to simplify running this example code

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,20 +1,12 @@
 (ns app.core
-  (:require ["/aws-exports" :default ^js aws-exports]
-            ["aws-amplify" :default Amplify]
-            [app.views :as views]
-            [reagent.dom :as rdom]
-            [app.auth :as auth]))
+  (:require [app.views :as views]
+            [reagent.dom :as rdom]))
 
 (def root-element (-> js/document (.getElementById "root")))
 
-(defn app []
-  [:> (-> views/panels
-          auth/with-auth)])
-
 (defn start []
-  (rdom/render [app] root-element))
+  (rdom/render [views/panels] root-element))
 
 (defn init []
   (-> js/console (.log "Initializing app"))
-  (-> Amplify (.configure aws-exports))
   (start))
