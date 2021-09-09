84e8b8cf41d4cd837afbacb13ff82ae83c617efe
davidvujic <david@vujic.se>
2021-09-09 16:11:51 +0200

    refactor: add border to user card

--- src/main/app/components/user.cljs
+++ src/main/app/components/user.cljs
@@ -3,5 +3,8 @@
             [reagent-material-ui.components :refer [box]]))
 
 (defn card [{:keys [name image]}]
-  [box {:sx {:p 4 :border "1px dashed grey"}}
+  [box {:sx {:p             4
+             :border        "1px solid #efefef"
+             :border-radius 2
+             :box-shadow    "2px 2px 2px grey"}}
    [greetings/welcome-with-avatar name image]])
