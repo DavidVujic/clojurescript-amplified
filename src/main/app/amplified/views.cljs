080f022edaa268678af3a687db1a8e7926f75424
davidvujic <david@vujic.se>
2021-09-10 09:25:29 +0200

    refactor: shorter syntax for re-frame subscribe

--- src/main/app/amplified/views.cljs
+++ src/main/app/amplified/views.cljs
@@ -6,6 +6,8 @@
             [re-frame.core :as rf]
             [reagent-material-ui.components :refer [stack]]))
 
+(def subscribe (comp deref rf/subscribe))
+
 (defn on-summary-change [^js e]
   (rf/dispatch [:app/user-summary (-> e .-target .-value)]))
 
@@ -13,7 +15,7 @@
   [stack {:spacing 2}
    [top/bar {:message   "Clojure. Amplified."
              :logout-fn #(auth/sign-out)}]
-   [user/card {:name    @(rf/subscribe [:app/user-name])
-               :image   @(rf/subscribe [:app/user-image])
-               :summary @(rf/subscribe [:app/user-summary])}
+   [user/card {:name    (subscribe [:app/user-name])
+               :image   (subscribe [:app/user-image])
+               :summary (subscribe [:app/user-summary])}
     {:on-summary-change on-summary-change}]])
