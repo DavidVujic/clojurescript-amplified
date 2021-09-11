223196ef79ecf8e99df433a849424c10b0d3f1c5
davidvujic <david@vujic.se>
2021-09-11 11:08:31 +0200

    fix: print change event data in the Storybook Actions tab

--- src/stories/app/stories/user_card_stories.cljs
+++ src/stories/app/stories/user_card_stories.cljs
@@ -8,23 +8,25 @@
                      :component user/card
                      :args      {:user-name "David"
                                  :image-url "https://www.gravatar.com/avatar/49e43b90a0eb475326df9e8b6dfbabcd"}
-                     :argTypes  {:on-save {:action "clicked on save!"}}}))
+                     :argTypes  {:on-save   {:action "clicked on save!"}
+                                 :on-change {:action "change"}}}))
 
 (def about (reagent/atom "This is a placeholder text"))
 
-(defn on-change [^js e]
-  (reset! about (-> e .-target .-value)))
+(defn change-fn [^js e action-fn]
+  (reset! about (-> e .-target .-value))
+  (action-fn @about))
 
 (defn user-card-component
   "It is possible to handle state within a story, by using an atom.
    The user/card component is extracted to a separate function to
    be able to deref the atom"
-  [{:keys [user-name image-url on-save]}]
+  [{:keys [user-name image-url on-save on-change]}]
   [user/card
    {:name    user-name
     :image   image-url
     :summary @about}
-   {:on-summary-change on-change
+   {:on-summary-change #(change-fn % on-change)
     :on-save           on-save}])
 
 (defn ^:export user-card [args]
