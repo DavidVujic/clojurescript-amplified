b23499f442ae983fee8f19e1fbc2c71a8a6feb4c
davidvujic <david@vujic.se>
2021-11-14 14:06:13 +0100

    feat: Example code using a custom docs view, according to Storybook 'doc blocks'

--- src/stories/app/stories/button_stories.cljs
+++ src/stories/app/stories/button_stories.cljs
@@ -1,13 +1,25 @@
 (ns app.stories.button-stories
   (:require [app.components.button :as button]
             [app.stories.helper :as helper]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            ["@storybook/addon-docs" :refer [Title
+                                             Subtitle
+                                             Description
+                                             Primary]]))
+
+(defn button-component-custom-docs []
+  [:<>
+   [:> Title]
+   [:> Subtitle "This is a custom subtitle"]
+   [:> Description {:type "INFO"} "Some details about the component"]
+   [:> Primary]])
 
 (def ^:export default
-  (helper/->default {:title     "A Save Button Component"
-                     :component button/save
-                     :args      {:visible? true}
-                     :argTypes {:on-click {:action "clicked!"}}}))
+  (helper/->default {:title      "A Save Button Component"
+                     :component  button/save
+                     :args       {:visible? true}
+                     :argTypes   {:on-click {:action "clicked!"}}
+                     :parameters {:docs {:page button-component-custom-docs}}}))
 
 (defn ^:export save-button [args]
   (let [params   (-> args helper/->params)
